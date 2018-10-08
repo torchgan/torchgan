7de4f82004eece087d347867dc6cd597f7a3fb92
Aniket Das <aniketd@iitk.ac.in>
2018-10-09 02:03:40 +0530

    Combine boundary equilibrium generator and discriminator loss

--- torchgan/losses/boundaryequilibrium.py
+++ torchgan/losses/boundaryequilibrium.py
@@ -2,12 +2,11 @@ import torch
 from .loss import GeneratorLoss, DiscriminatorLoss
 from ..utils import reduce
 
-__all__ = ['BoundaryEquilibriumGeneratorLoss',
-           'BoundaryEquilibriumDiscriminatorLoss']
+__all__ = ['BoundaryEquilibriumLoss']
 
 
-class BoundaryEquilibriumGeneratorLoss(GeneratorLoss):
-    r"""Boundary Equilibrium GAN generator loss from
+class BoundaryEquilibriumLoss(GeneratorLoss, DiscriminatorLoss):
+    r"""Boundary Equilibrium GAN generator and discriminator loss from
     `"BEGAN : Boundary Equilibrium Generative Adversarial Networks
     by Berthelot et. al." <https://arxiv.org/abs/1703.10717>`_ paper
 
@@ -33,7 +32,7 @@ class BoundaryEquilibriumGeneratorLoss(GeneratorLoss):
     """
     def __init__(self, reduction='elementwise_mean',
                  init_k=0.0, lambd=0.001, gamma=0.5):
-        super(BoundaryEquilibriumGeneratorLoss, self).__init__(reduction)
+        super(BoundaryEquilibriumLoss, self).__init__(reduction)
         self.lambd = lambd
         self.k = init_k
         self.gamma = gamma
@@ -55,66 +54,9 @@ class BoundaryEquilibriumGeneratorLoss(GeneratorLoss):
                                  (N, \*) where \* means any number of additional dimensions.
 
         Returns:
-            scalar if reduction is applied else Tensor with dimensions (N, \*).
+            scalar tuple if reduction is applied else Tensor tuple each with dimensions (N, \*).
         """
         ld = reduce(dx - self.k * dgz, self.reduction)
         lg = reduce(dgz, self.reduction)
         self.k += self.lambd * (self.gamma * ld - lg)
-        return lg
-
-
-class BoundaryEquilibriumDiscriminatorLoss(DiscriminatorLoss):
-    r"""Boundary Equilibrium GAN generator loss from
-    `"BEGAN : Boundary Equilibrium Generative Adversarial Networks
-    by Berthelot et. al." <https://arxiv.org/abs/1703.10717>`_ paper
-
-    The loss can be described as
-
-    .. math:: L(D) = D(x) - k_t \times D(G(z))
-    .. math:: L(G) = D(G(z))
-    .. math:: k_{t+1} = k_t + \lambda \times (\gamma \times D(x) - D(G(z)))
-
-    where
-
-    - G : Generator
-    - D : Discriminator
-    - :math:`k_t` : Running average of the balance point of G and D
-    - :math:`\lambda` : Learning rate of the running average
-    - :math:`\gamma` : Goal bias hyperparameter
-
-    Args:
-        reduction (string, optional): Specifies the reduction to apply to the output.
-            If `none` no reduction will be applied. If `elementwise_mean` the sum of
-            the elements will be divided by the number of elements in the output. If
-            `sum` the output will be summed.
-    """
-    def __init__(self, reduction='elementwise_mean',
-                 init_k=0.0, lambd=0.001, gamma=0.5):
-        super(BoundaryEquilibriumDiscriminatorLoss, self).__init__(reduction)
-        self.lambd = lambd
-        self.k = init_k
-        self.gamma = gamma
-
-    def set_k(self, k=0.0):
-        r"""Change the default value of k
-
-        Args:
-            k (float, optional) : New value to be set.
-        """
-        self.k = k
-
-    def forward(self, dx, dgz):
-        r"""
-        Args:
-            dx (torch.Tensor) : Output of the Discriminator. It must have the dimensions
-                                (N, \*) where \* means any number of additional dimensions.
-            dgz (torch.Tensor) : Output of the Generator. It must have the dimensions
-                                 (N, \*) where \* means any number of additional dimensions.
-
-        Returns:
-            scalar if reduction is applied else Tensor with dimensions (N, \*).
-        """
-        ld = reduce(dx - self.k * dgz, self.reduction)
-        lg = reduce(dgz, self.reduction)
-        self.k += self.lambd * (self.gamma * ld - lg)
-        return ld
+        return lg, ld
