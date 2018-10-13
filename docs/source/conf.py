b572f42448553e0ba55ea6820b607ce0d2d530b2
Avik Pal <avikpal@iitk.ac.in>
2018-10-13 23:18:20 +0530

    Sphinx Autobuild (#29)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,8 @@
 #
 import os
 import sys
-sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../..'))
 import sphinx_rtd_theme
 
 
