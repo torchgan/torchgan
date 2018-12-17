f5f6f5d14fea155e69b7b532d90d8cfbf6bd58af
Avik Pal <avikpal@iitk.ac.in>
2018-12-18 01:11:16 +0530

    Add a transposed residual block (#69)
    * Add a transposed residual block
    
    * Style fixes
    
    * Change nonlinearity and fix naming issues
    
    * Remove super init and reuse forward pass

--- torchgan/layers/residual.py
+++ torchgan/layers/residual.py
@@ -1,11 +1,12 @@
 import torch.nn as nn
 
-__all__ = ['ResidualBlock']
+__all__ = ['ResidualBlock2d', 'ResidualBlockTranspose2d']
 
-class ResidualBlock(nn.Module):
-    def __init__(self, filters, kernels, strides=None, paddings=None, activation=nn.ReLU,
+class ResidualBlock2d(nn.Module):
+    def __init__(self, filters, kernels, strides=None, paddings=None, nonlinearity=None,
                  batchnorm=True, shortcut=None):
-        super(ResidualBlock, self).__init__()
+        super(ResidualBlock2d, self).__init__()
+        nl = nn.LeakyReLU(0.2) if nonlinearity is None else nonlinearity
         if strides is None:
             strides = [1 for _ in range(len(kernels))]
         if paddings is None:
@@ -18,15 +19,40 @@ class ResidualBlock(nn.Module):
             if batchnorm:
                 layers.append(nn.BatchNorm2d(filters[i]))
             if i != len(filters):  # Last layer does not get an activation
-                layers.append(activation())
+                layers.append(nl)
         self.layers = nn.Sequential(*layers)
         self.shortcut = shortcut
 
-    def forward(self, x, activation=None):
+    def forward(self, x, nonlinearity=None):
         out = self.layers(x)
         if self.shortcut is not None:
             out += self.shortcut(x)
-        if activation is None:
-            return out
-        else:
-            return activation(out)
+        return out if nonlinearity is None else nonlinearity(out)
+
+class ResidualBlockTranspose2d(nn.Module):
+    def __init__(self, filters, kernels, strides=None, paddings=None, nonlinearity=None,
+                 batchnorm=True, shortcut=None):
+        super(ResidualBlockTranspose2d, self).__init__()
+        nl = nn.LeakyReLU(0.2) if nonlinearity is None else nonlinearity
+        if strides is None:
+            strides = [1 for _ in range(len(kernels))]
+        if paddings is None:
+            paddings = [0 for _ in range(len(kernels))]
+        assert len(filters) == len(kernels) + 1 and len(filters) == len(strides) + 1 \
+            and len(filters) == len(paddings) + 1
+        layers = []
+        for i in range(1, len(filters)):
+            layers.append(nn.ConvTranspose2d(filters[i - 1], filters[i], kernels[i - 1],
+                                             strides[i - 1], paddings[i - 1]))
+            if batchnorm:
+                layers.append(nn.BatchNorm2d(filters[i]))
+            if i != len(filters):  # Last layer does not get an activation
+                layers.append(nl)
+        self.layers = nn.Sequential(*layers)
+        self.shortcut = shortcut
+
+    def forward(self, x, nonlinearity=None):
+        out = self.layers(x)
+        if self.shortcut is not None:
+            out += self.shortcut(x)
+        return out if nonlinearity is None else nonlinearity(out)
